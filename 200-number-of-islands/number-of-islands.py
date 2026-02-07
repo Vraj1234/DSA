288ffaa78d9d128c2e6989adb82cca1b129a0437
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-02-07 13:58:12 -0800

    Time: 249 ms (47.52%) | Memory: 22.2 MB (33.94%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,11 +2,11 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        vis = [[False]*cols for _ in range(rows)]
-        print(vis)
+        vis = [[False] * cols for _ in range(rows)]
+        ctr = 0
 
         def dfs(i,j):
-            if i<0 or i>=rows or j<0 or j>=cols or vis[i][j] == True or grid[i][j] == "0":
+            if i<0 or i>= rows or j<0 or j>= cols or vis[i][j] == True or grid[i][j] == '0':
                 return
             
             vis[i][j] = True
@@ -14,13 +14,11 @@ class Solution:
             dfs(i-1, j)
             dfs(i, j+1)
             dfs(i+1, j)
-    
-        ctr = 0
 
         for i in range(rows):
             for j in range(cols):
-                if(not vis[i][j] and grid[i][j] == "1"):
+                if grid[i][j] == '1' and vis[i][j] == False:
                     dfs(i,j)
                     ctr+=1
-        
+                
         return ctr
