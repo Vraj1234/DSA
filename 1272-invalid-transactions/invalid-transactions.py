4c4556d052492e76148858a70ae07c3dd55ab0de
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-02-19 19:28:53 -0800

    Time: 459 ms (5.07%) | Memory: 19.7 MB (74.52%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,33 +1,27 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        records = defaultdict(list)
         res = []
+        by_name = defaultdict(list)
         c = Counter(transactions)
 
-        for x in transactions:
-            name, time, amt, city = x.split(',')
-            records[name].append([int(time), city])
-            if int(amt) > 1000 and res.count(x) < c[x]:
-                res.append(x)
+        for txn in transactions:
+            name, t, a, city = txn.split(',')
+            time = int(t)
+            amount = int(a)
+            if amount > 1000 and res.count(txn) < c[txn]:
+                res.append(txn)
+            by_name[name].append((time, city))
         
-        for x in transactions:
-            name, time, amt, city = x.split(',')
-            val = records[name]
-            val.sort(key = lambda x: x[0])
-            print(val)
-            for v in val:
-                diff = abs(v[0] - int(time))
-                if diff <= 60 and v[1] != city and res.count(x) < c[x]:
-                    res.append(x)
-                
+
+        for txn in transactions:
+            cur_name, t, a, cur_city = txn.split(',')
+            cur_time = int(t)
+            cur_amount = int(a)
+            ar = by_name[cur_name]
+            for pair in ar:
+                time, city = pair
+                if abs(cur_time - time) <= 60 and city != cur_city and res.count(txn) < c[txn]:
+                    res.append(txn)
+        
         return res
 
-
-
-        
-        
-
-        
-
-
-            
