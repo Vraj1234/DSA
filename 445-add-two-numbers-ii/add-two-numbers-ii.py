2d4ac95e96f3cd31db1a6c1a086fdaf73e4e174d
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-02-19 18:39:21 -0800

    Time: 7 ms (39.38%) | Memory: 19.3 MB (65.20%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/add-two-numbers-ii.py
@@ -5,56 +5,39 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
-        def rev(l):
-            if l is None:
-                return None
-            if l.next is None:
-                return l
-            if l.next.next is None:
-                n = l.next
-                l.next = None
-                n.next = l
-                return n
-            c = l.next
-            p = l
-            n = l.next.next
-            p.next = None
-            c.next = p
-            while n is not None:
-                c.next = p
-                p = c
-                c = n
-                n = n.next
-            c.next = p
-            return c
-        
-        rl1 = rev(l1)
-        rl2 = rev(l2)
-
+        s1 = []
+        s2 = []
         carry = 0
-        head = ListNode(-1)
-        temp = head
-        while rl1 or rl2: #v1 = 0, v2 = 0, carry = 0
-            v1 = rl1.val if rl1 else 0
-            v2 = rl2.val if rl2 else 0
-            s = v1+ v2 + carry #7
-            if carry:
-                carry = 0
-            if s>9:
-                s = s%10 #s = 0
-                carry = 1
-            temp.next = ListNode(s)
-            temp = temp.next
-            if rl1:
-                rl1 = rl1.next
-            if rl2:
-                rl2 = rl2.next
+        nxt = None
+        head = None
+        t1 = l1
+        t2 = l2
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+        
+        l1 = t1
+        l2 = t2
+        while s1 or s2 or carry:
+            s = carry
+            if s1:
+                s+=s1.pop()
+            if s2:
+                s+= s2.pop()
             
-        if carry:
-            temp.next = ListNode(carry)
-            temp = temp.next
-        print(head.next)
-        return rev(head.next)
-    
+            if not nxt:
+                nxt = ListNode(s%10)
+            else:
+                new_node = ListNode(s%10)
+                new_node.next = nxt
+                nxt = new_node
+            
+            head = nxt
+            carry = s//10
+        
+        return head
 
