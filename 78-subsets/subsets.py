7a09e391a1d373965930b37549ac3f9c42eace23
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-05-25 16:18:56 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.97%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,19 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        i = 0
-        temp =[]
-        res = []
-        n = len(nums)
-        def rec(i, nums, temp):
-            if i == n:
-                res.append(temp[:])
+        self.res = []
+        temp = []
+        def recur(i, nums, temp):
+            if i == len(nums):
+                self.res.append(temp[:])
                 return
-            
-            rec(i+1, nums, temp)#dont add
+            recur(i+1, nums, temp)
             temp.append(nums[i])
-            rec(i+1, nums, temp)
+            recur(i+1, nums, temp)
             temp.pop()
-
-        rec(0,nums, [])
         
-        return res
+        recur(0,nums,[])
+        return self.res
