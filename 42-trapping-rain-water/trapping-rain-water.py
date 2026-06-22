9ac1bbf4384e8ccda47656da42e3414575cfbe38
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-06-22 08:50:32 -0700

    Time: 12 ms (32.43%) | Memory: 20.9 MB (83.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,9 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        prefix = list(accumulate(height, max))
-        suffix = list(accumulate(height[::-1], max))[::-1]
-        count = 0
-        for i,x in enumerate(height):
-            count+= abs(x - min(prefix[i], suffix[i]))
+        res = 0
+        l,r = 0, len(height)-1
+        lmax, rmax = height[0], height[-1]
+
+        while l<r:
+            if lmax<=rmax:
+                l+=1
+                if height[l]<=lmax:
+                    res= res+(lmax - height[l])
+                lmax = max(lmax, height[l])
+            else:
+                r-=1
+                if height[r]<=rmax:
+                    res= res+(rmax - height[r])
+                rmax = max(rmax, height[r])
         
-        return count
+        return res
