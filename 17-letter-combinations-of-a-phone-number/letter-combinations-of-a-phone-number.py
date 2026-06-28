f04261e24f6c545a4183f98f3932cab7276f8241
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-06-27 20:20:40 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (39.75%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,29 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         mapping = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"]
+            2: "abc",
+            3: "def",
+            4: "ghi",
+            5: "jkl",
+            6: "mno",
+            7: "pqrs",
+            8: "tuv",
+            9: "wxyz",
         }
 
         res = []
-        temp = ""
-
-        def bt(i, temp):
-            if i == len(digits):
-                res.append(temp[:])
+        n = len(digits)
+        def dfs(temp, i):
+            if i == n:
+                res.append(temp)
                 return
             
-            num = digits[i]
-            for ch in mapping[num]:
-                temp+=ch
-                bt(i+1, temp)
+            for ch in mapping[int(digits[i])]:
+                temp += ch
+                dfs(temp, i+1)
                 temp = temp[:-1]
-            
-        bt(0,"")
+        
+        dfs("", 0)
         return res
+            
