49e58c06ab6887990ccbe1ee911360043c290ea6
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-24 18:25:50 -0700

    Time: 3 ms (61.63%) | Memory: 19.4 MB (25.37%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,21 +1,30 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp = [-1]*(len(cost)+1)
-
-        def f(n):
-            if n == 0:
-                return cost[0]
-            if n == 1:
-                return cost[1]
+        n = len(cost)
+        dp = [-1]*(n+1)
+        dp[0], dp[1] = cost[0], cost[1]
+        for i in range(2,n+1):
+            dp[i] = min(dp[i-1], dp[i-2])
+            if i<n:
+                dp[i] += cost[i]
+        
+        return dp[n]
             
-            if dp[n] != -1:
-                return dp[n]
 
-            left = f(n-1)
-            right = f(n-2)
-            res = min(left, right)
-            dp[n] = res + cost[n] if n<len(cost) else res
+        # def f(n):
+        #     if n == 0:
+        #         return cost[0]
+        #     if n == 1:
+        #         return cost[1]
+            
+        #     if dp[n] != -1:
+        #         return dp[n]
 
-            return dp[n]
+        #     left = f(n-1)
+        #     right = f(n-2)
+        #     res = min(left, right)
+        #     dp[n] = res + cost[n] if n<len(cost) else res
+
+        #     return dp[n]
         
         return f(len(cost))
