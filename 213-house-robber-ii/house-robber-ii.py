24f87c48466156ef3eb661edc27081586061cea0
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-24 19:07:20 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (68.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,28 +1,22 @@
 class Solution:
-
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
 
-        def HR1(arr):
-            n = len(arr)
-            if len(arr) == 1:
-                return arr[0]
-            
-            # dp = [-1]*(n)
-            p2 = arr[0]
-            p1 = max(arr[0], arr[1])
-
-            for i in range(2, n):
-                pick = arr[i] + p2
-                non_pick = p1
-                curi = max(pick, non_pick)
-                p2 = p1
-                p1 = curi
-
-            return p1
-
-        m1 = HR1(nums[:-1])
-        m2 = HR1(nums[1:])
-        return max(m1, m2)
+        def get_cost(ls):
+            n = len(ls)
+            if n == 0:
+                return 0
+            if n<3:
+                return max(ls)
+            p1, p2 = ls[0], max(ls[0], ls[1])
+            for i in range(2, n+1):
+                if i < n:
+                    res = max(p2, p1 + ls[i])
+                else:
+                    res = max(p2, p1)
+                p1 = p2
+                p2 = res
+            return p2
         
+        return max(get_cost(nums[:-1]), get_cost(nums[1:]))
