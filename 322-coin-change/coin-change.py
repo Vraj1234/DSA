6bfd763749909e2c3f8ff7bbb440d970a914b25a
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-25 15:38:28 -0700

    Time: 531 ms (47.13%) | Memory: 19.4 MB (98.81%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,22 +1,36 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [-1]*(amount+1)
-
-        def f(n):
-            if n == 0:
-                return 1
-            if n<0:
-                return float('inf')
-            
-            if dp[n] != -1:
-                return dp[n]
-
+        dp = [float('inf')]*(amount+1)
+        dp[0] = 1
+        for i in range(1, amount+1):
             minimum = float('inf')
             for diff in coins:
-                minimum = min(minimum, f(n-diff))
-
-            dp[n] = 1 + minimum
-            return dp[n]
+                temp = 0
+                if i-diff < 0:
+                    temp = float('inf')
+                else:
+                    temp = dp[i-diff]
+                minimum = min(minimum, temp)
+            dp[i] = 1 + minimum
         
-        result = f(amount)
-        return -1 if result == float('inf') else result-1
+        return dp[amount]-1 if dp[amount] != float('inf') else -1
+
+        # def f(n):
+        #     if n == 0:
+        #         return 1
+        #     if n<0:
+        #         return float('inf')
+            
+        #     if dp[n] != -1:
+        #         return dp[n]
+
+        #     minimum = float('inf')
+        #     for diff in coins:
+
+        #         minimum = min(minimum, f(n-diff))
+
+        #     dp[n] = 1 + minimum
+        #     return dp[n]
+        
+        # result = f(amount)
+        # return -1 if result == float('inf') else result-1
