52bb9b36627a1473c6059318ac0c658ef1b262f0
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-24 18:21:39 -0700

    Time: 7 ms (15.81%) | Memory: 20.7 MB (16.40%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,45 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        #memoization
-        # def recur(n):
-        #     if n<=1:
-        #         return 0
+        dp = [-1]*(len(cost)+1)
+
+        def f(n):
+            if n == 0:
+                return cost[0]
+            if n == 1:
+                return cost[1]
             
-        #     if dp[n]!= -1:
-        #         return dp[n]
+            if dp[n] != -1:
+                return dp[n]
 
-        #     left = recur(n-1) + cost[n-1]
-        #     right = recur(n-2) + cost[n-2]
-            
-        #     dp[n] = min(left, right)
-        #     return dp[n]
+            left = f(n-1)
+            right = f(n-2)
+            res = min(left, right)
+            dp[n] = res + cost[n] if n<len(cost) else res
+
+            return dp[n]
         
-        # n= len(cost)
-        # dp = [-1]*(n+1)
-        # return recur(n)
-
-        #Tabulation
-        # n= len(cost)
-        # dp = [-1]*(n+1)
-        # dp[0], dp[1] = 0,0
-        # for i in range(2, n+1):
-        #     left = dp[i-1] + cost[i-1] 
-        #     right = dp[i-2] + cost[i-2]
-        #     dp[i] = min(left, right)
-        
-        # return dp[n]
-    
-        #Space optimization
-        n= len(cost)
-        p2, p1 = 0,0
-        for i in range(2, n+1):
-            left = p1 + cost[i-1] 
-            right = p2 + cost[i-2]
-            cur = min(left, right)
-            p2 = p1
-            p1 = cur
-        
-        return p1
-
-
-            
+        return f(len(cost))
