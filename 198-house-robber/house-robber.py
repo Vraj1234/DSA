90e94859fca2fd100e09a205e192fe0d246bc4a4
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-24 18:55:34 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.37%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,48 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        # dp = [-1]*(n)
+        if n<3:
+            return max(nums)
+        dp = [-1] * (n+1)
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, n+1):
+            if i<n:
+                dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+            else:
+                dp[i] = max(dp[i-1], dp[i-2])
+        return dp[n]
+
         # def f(n):
-        #     if n ==0:
+        #     if n == 0:
         #         return nums[0]
-        #     if n<0:
-        #         return 0
+        #     if n == 1:
+        #         return max(nums[0], nums[1])
+
         #     if dp[n] != -1:
         #         return dp[n]
-
-        #     pick = nums[n]+ f(n-2)
-        #     non_pick = 0 + f(n-1)
-        #     dp[n] = max(pick, non_pick)
+        #     left = f(n-1) + 0
+        #     right = f(n-2) + nums[n]
+        #     dp[n] = max(left, right)
         #     return dp[n]
-        
+
         # return f(len(nums)-1)
-
-        # if len(nums) == 1:
-        #     return nums[0]
-        
-        # dp = [-1]*(n)
-        # dp[0] = nums[0]
-        # dp[1] = max(nums[0], nums[1])
-
-        # for i in range(2, n):
-        #     pick = nums[i] + dp[i-2]
-        #     non_pick = dp[i-1]
-        #     dp[i] = max(pick, non_pick)
-
-        # return dp[n-1]
-
-        if len(nums) == 1:
-            return nums[0]
-        
-        # dp = [-1]*(n)
-        p2 = nums[0]
-        p1 = max(nums[0], nums[1])
-
-        for i in range(2, n):
-            pick = nums[i] + p2
-            non_pick = p1
-            curi = max(pick, non_pick)
-            p2 = p1
-            p1 = curi
-
-        return p1
