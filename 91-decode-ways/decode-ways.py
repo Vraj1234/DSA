d364943e634bc36295411d60bd83be075c01a905
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-07-27 12:27:11 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.38%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,13 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [-1]*(n+1)
-
-        def f(i): 
-            if i >= n:
+        dp = [-1] * (n+1)
+        def f(i):
+            if i == n:
+                dp[i] = 1
                 return 1
 
             if s[i] == "0":
+                dp[i] = 0
                 return 0
 
             if dp[i] != -1:
@@ -15,15 +16,9 @@ class Solution:
 
             left = f(i+1)
             right = 0
-            if i+1<n:
-                temp = s[i:i+2]
-                num = int(temp)
-                if 10<= num <=26:
-                    right = f(i+2)
-
-            dp[i] = left+right
+            if i+1<n and 10 <= int(s[i:i+2]) <=26:
+                right = f(i+2)
+            dp[i] = left+ right
             return dp[i]
         
-        result = f(0)
-        # print("Filled DP Array:", dp)  # <--- Print AFTER f(0) finishes
-        return result
+        return f(0)
