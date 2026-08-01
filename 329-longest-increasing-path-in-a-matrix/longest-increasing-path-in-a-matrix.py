8778017737a50876cb1464f6555d5cbe00db7365
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-01 13:07:58 -0700

    Time: 155 ms (27.60%) | Memory: 22.2 MB (58.54%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -2,30 +2,31 @@ class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         R,C = len(matrix), len(matrix[0])
         vis = set()
+        op = [[-1]*C for _ in range(R)]
         dp = [[-1]*C for _ in range(R)]
-        res = 0
+        res = float('-inf')
+
         def dfs(r,c, prevr, prevc):
             if r<0 or r>=R or c<0 or c>=C:
-                return 0 
-            
+                return 0
             if (r,c) in vis:
                 return 0
-            
-            if prevr!= -1 and matrix[r][c] <= matrix[prevr][prevc]:
+            if prevr != -1 and matrix[r][c] <= matrix[prevr][prevc]:
                 return 0
             
             if dp[r][c] != -1:
                 return dp[r][c]
-                
             vis.add((r,c))
-             #right
-            res =  1 + max( dfs(r-1, c, r, c), dfs(r+1, c, r, c), dfs(r, c-1, r, c), dfs(r, c+1, r, c))
-            vis.remove((r,c))
+            res = 1 + max(dfs(r-1,c,r,c),dfs(r+1,c,r,c),dfs(r,c-1,r,c),dfs(r,c+1,r,c))
             dp[r][c] = res
+            vis.remove((r,c))
             return res
             
+
         for r in range(R):
             for c in range(C):
                 res = max(res, dfs(r,c, -1, -1))
         
+        # SC = O(m+n)
+        # TC = mxn(4^(m+n))
         return res
