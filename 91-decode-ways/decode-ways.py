523ba2a24e13ed319aeba326b09bed4307a24d66
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-04 14:42:26 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (70.65%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -2,23 +2,33 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
         dp = [-1]*(n+1)
-        def f(i):
-            if i == n:
-                dp[i] = 1
-                return 1
-
+        dp[n] = 1
+        for i in range(n-1, -1, -1):
             if s[i] == "0":
-                return 0
-
-            if dp[i] != -1:
-                return dp[i]
-
-            left = f(i+1)
+                dp[i] = 0
+                continue
+            left = dp[i+1]
             right = 0
-            if i+1<n and 10<= int(s[i:i+2])<=26:
-                right = f(i+2)
-            
+            if i+1<n and 10<=int(s[i:i+2])<=26:
+                right = dp[i+2]
             dp[i] = left+right
-            return left+right
-        
-        return f(0)
+        return dp[0]
+            
+        # def f(i):
+        #     if i == n:
+        #         return 1
+            
+        #     if i >= n:
+        #         return 0
+
+        #     if s[i] == "0":
+        #         return 0
+
+        #     left = f(i+1)
+        #     right = 0
+        #     if i+1<n and 10<=int(s[i:i+2])<=26:
+        #         right = f(i+2)
+            
+        #     return left+right
+
+        # return f(0)
