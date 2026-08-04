a95919ca7afcdb8a2a2ffdd5e4f77dacda1b79fb
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-04 14:08:05 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (52.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,20 +1,28 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n<=1:
+            return 1
         dp = [-1]*(n+1)
-        # dp[0], dp[1] = 
-
-        def f(i):
-            if i == 0 or i == 1:
-                dp[i] = 1
-                return 1
-            
-            if dp[i] != -1:
-                return dp[i]
-
-            left = f(i-1)
-            right = f(i-2)
-
+        dp[0], dp[1] = 1,1
+        for i in range(2, n+1):
+            left = dp[i-1]
+            right = dp[i-2]
             dp[i] = left+right
-            return left+right
         
-        return f(n)
+        return dp[n]
+
+        # def f(i):
+        #     if i == 0 or i == 1:
+        #         dp[i] = 1
+        #         return 1
+            
+        #     if dp[i] != -1:
+        #         return dp[i]
+
+        #     left = f(i-1)
+        #     right = f(i-2)
+
+        #     dp[i] = left+right
+        #     return left+right
+        
+        # return f(n)
