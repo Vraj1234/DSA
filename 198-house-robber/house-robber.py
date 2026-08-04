ea311a80db604253b0d391c1f46b3a6adc1cc698
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-04 15:17:10 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.07%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,30 +1,36 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
         n = len(nums)
-        if n<3:
-            return max(nums)
-
-        p1, p2 = nums[0], max(nums[0], nums[1])
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            max(nums[0], nums[1])
+        dp = [-1]*(n+1)
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
         for i in range(2, n+1):
+            left = dp[i-1] 
+            right = 0
             if i<n:
-                res = max(p2, p1 + nums[i])
-            else:
-                res = max(p2, p1)
-            p1 = p2
-            p2 = res
-        return p2
+                right = nums[i] + dp[i-2]
+            dp[i] = max(left,right)
 
-        # def f(n):
-        #     if n == 0:
+        return dp[n]
+        # def f(i):
+        #     if i == 0:
         #         return nums[0]
-        #     if n == 1:
+            
+        #     if i == 1:
         #         return max(nums[0], nums[1])
+            
+        #     if i<0:
+        #         return float('-inf')
 
-        #     if dp[n] != -1:
-        #         return dp[n]
-        #     left = f(n-1) + 0
-        #     right = f(n-2) + nums[n]
-        #     dp[n] = max(left, right)
-        #     return dp[n]
-
-        # return f(len(nums)-1)
+        #     left = f(i-1)
+        #     right = 0
+        #     if i<n:
+        #         right = nums[i] + f(i-2)
+        
+        #     return max(left, right)
+        
+        # return f(len(nums))
