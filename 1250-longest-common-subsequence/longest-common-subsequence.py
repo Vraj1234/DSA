64358654be637b67384a503eae27e77e0391a213
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-07 15:26:42 -0700

    Time: 702 ms (21.01%) | Memory: 46.5 MB (21.03%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,15 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        dp = [[None]*len(s2) for _ in range(len(s1))]
+        n,m = len(s1), len(s2)
+        dp = [[None]*(m+1) for _ in range(n+1)]
+
 
         def f(i,j):
-            if i<0 or j<0:
+            if i==0 or j==0:
                 return 0
 
             if dp[i][j] != None:
                 return dp[i][j]
 
-            if s1[i] == s2[j]:
+            if s1[i-1] == s2[j-1]:
                 v = 1 + f(i-1, j-1)
                 dp[i][j] = v
                 return v
@@ -18,4 +20,4 @@ class Solution:
             dp[i][j] = t
             return t
 
-        return f(len(s1)-1, len(s2)-1)
+        return f(n,m)
