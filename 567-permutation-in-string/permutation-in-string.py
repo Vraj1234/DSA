3a1905162053f410490c74885d8136cb6bf72a9e
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-07 17:49:14 -0700

    Time: 23 ms (48.08%) | Memory: 19.5 MB (18.28%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,24 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+        if len(s2)< len(s1):
             return False
-        c = Counter(s1)
-        l,r = 0, len(s1)-1
-        freq = defaultdict(int)
-        for x in range(l,r+1):
-            freq[s2[x]]+=1
 
-        while r < len(s2):
-            print(freq)
-            print(c)
-            if(freq == c):
+        cur = defaultdict(int)
+        l,r = 0, 0
+        check = Counter(s1)
+        
+        while r<len(s1):
+            cur[s2[r]]+=1
+            r+=1
+        r-=1
+
+        if check == cur:
+            return True
+
+        while r<len(s2):
+            #print(s1, s2[l:r+1])
+            if check == cur:
                 return True
-            if freq[s2[l]] == 1:
-                del freq[s2[l]]
-            else:
-                freq[s2[l]]-=1
+            cur[s2[l]]-=1
+            if cur[s2[l]] == 0:
+                del cur[s2[l]]
             l+=1
             r+=1
-            if r< len(s2): 
-                freq[s2[r]]+=1
+            if r<len(s2):
+                cur[s2[r]]+=1
+        
         return False
+
