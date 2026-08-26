6e02398003cfcca0ae4b878d9ca73ab9042363c8
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-25 18:58:00 -0700

    Time: 684 ms (32.66%) | Memory: 30.3 MB (20.73%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,32 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [-1]*(amount+1)
-        dp[0] = 0
-        for i in range(1, amount+1):
-            res = float('inf')
-            for idx in range(len(coins)):
-                val = float('inf') if i-coins[idx]<0 else dp[i-coins[idx]]
-                res = min(res, val)
-            dp[i] = 1+res
-        return dp[amount] if dp[amount] != float('inf') else -1
 
-        # def f(i):
-        #     if i<0:
-        #         return float('inf')
-                
-        #     if i == 0:
-        #         dp[i] = 0
-        #         return 0
-
-        #     if dp[i] != -1:
-        #         return dp[i]
-
-        #     res = float('inf')
-        #     for idx in range(len(coins)):
-        #         res = min(res, f(i-coins[idx]))
+        def f(amount):
+            if amount == 0:
+                return 0
             
-        #     dp[i] = 1+res
-        #     return 1+res
-        
-        # result = f(amount)
-        # return -1 if result ==float('inf') else result
+            if amount < 0:
+                return float('inf')
+
+            if dp[amount]!= -1:
+                return dp[amount]
+
+            res = float('inf')
+            for coin in coins:
+                val = f(amount-coin)
+                res = min(res, val)
+            
+            dp[amount] = 1+ res
+            return 1+ res
+
+        result = f(amount)
+
+        return -1 if result == float('inf') else result
