72c18a20870b8d89ac1063b9c4b65e75b719283c
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-08-27 20:25:04 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.14%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -2,20 +2,36 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         R,C = m,n
         dp = [[-1]*C for _ in range(R)]
-        def f(r,c):
-            if r==0 and c ==0:
-                return 1
-            if r<0 or c<0:
-                return 0
+        dp[0][0] = 1
+        for r in range(R):
+            for c in range(C):
+                if r==0 and c==0:
+                    continue
+                if c-1<0:
+                    left = 0
+                else:
+                    left = dp[r][c-1]
+                if r-1<0:
+                    top = 0
+                else:
+                    top = dp[r-1][c]
+                dp[r][c] = top + left       
+        return dp[R-1][C-1]
 
-            if dp[r][c] != -1:
-                return dp[r][c]
+        # def f(r,c):
+        #     if r==0 and c ==0:
+        #         return 1
+        #     if r<0 or c<0:
+        #         return 0
 
-            top = f(r-1,c)
-            left = f(r,c-1)
+        #     if dp[r][c] != -1:
+        #         return dp[r][c]
 
-            dp[r][c] = top+left
+        #     top = f(r-1,c)
+        #     left = f(r,c-1)
 
-            return dp[r][c]
+        #     dp[r][c] = top+left
 
-        return f(R-1, C-1)
+        #     return dp[r][c]
+
+        # return f(R-1, C-1)
