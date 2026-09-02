fd1a50276113e7e9fa572447a909413af29af759
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-09-02 16:30:12 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (24.55%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,36 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            max(nums[0], nums[1])
-        dp = [-1]*(n+1)
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, n+1):
-            left = dp[i-1] 
-            right = 0
-            if i<n:
-                right = nums[i] + dp[i-2]
-            dp[i] = max(left,right)
+        dp = [-1]* (len(nums)+1)
+        def f(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])
 
-        return dp[n]
-        # def f(i):
-        #     if i == 0:
-        #         return nums[0]
-            
-        #     if i == 1:
-        #         return max(nums[0], nums[1])
-            
-        #     if i<0:
-        #         return float('-inf')
+            if dp[i] != -1:
+                return dp[i]
 
-        #     left = f(i-1)
-        #     right = 0
-        #     if i<n:
-        #         right = nums[i] + f(i-2)
+            pick = nums[i] + f(i-2)
+            not_pick = 0 + f(i-1)
         
-        #     return max(left, right)
+            dp[i] = max(pick, not_pick)
+            return max(pick, not_pick)
         
-        # return f(len(nums))
+        return f(len(nums)-1)
