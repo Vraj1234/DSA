ae38d177a2be790f544feb81fb7de674c1d2800d
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-09-06 16:53:04 -0700

    Time: 131 ms (27.06%) | Memory: 19.9 MB (11.73%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,37 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-
-        n = len(s)
-        l,r = 0,0
-        check = Counter(t)
-        cur = defaultdict(int)
-        res = float('inf')
-        res_idx = [-1, -1]
+        l = r = 0
+        target = Counter(t)
         match = 0
-
+        n = len(s)
+        map = defaultdict(int)
+        res = float('inf')
+        res_word = ""
+        
         while r<n:
-            #expansion
-            while r<n and match != len(check):
-                if s[r] in check:
-                    cur[s[r]]+=1
-                    if cur[s[r]] == check[s[r]]:
+            while r<n and match != len(target):
+                if s[r] in target:
+                    map[s[r]]+=1
+                    if map[s[r]] == target[s[r]]:
                         match+=1
                 r+=1
 
-            #contraction
-            while match == len(check):
-                if r-l < res:
-                    res = min(res, (r-l))
-                    res_idx = [l,r]
-                if s[l] in check:
-                    cur[s[l]]-=1
-                    if cur[s[l]] < check[s[l]]:
+            while l<r and match == len(target):
+                if r-l< res:
+                    res = r-l
+                    res_word = s[l:r]
+                if s[l] in map:
+                    map[s[l]]-=1
+                    if map[s[l]] < target[s[l]]:
                         match-=1
                 l+=1
-        
-        return s[res_idx[0]:res_idx[1]]
-
-
-
+                
+        return res_word
+                
