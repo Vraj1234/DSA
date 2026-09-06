80a9d510aad205489080a0d43a5fbdcd0ca11afb
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-09-05 18:55:55 -0700

    Time: 133 ms (20.42%) | Memory: 35 MB (27.80%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,18 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        if k == n:
-            return nums
-
-        k = k%n
-
         def reverse(l,r):
+            # print("Entering")
             while l<r:
                 nums[l], nums[r] = nums[r], nums[l]
                 l+=1
                 r-=1
+            # print("exiting")
+            return
+
+        n = len(nums)
+        if k>len(nums):
+            k = k%n
         
+        reverse(n-k, n-1)
+        reverse(0, n-k-1)
         reverse(0, n-1)
-        reverse(0,k-1)
-        reverse(k, n-1)
+        return
+        
