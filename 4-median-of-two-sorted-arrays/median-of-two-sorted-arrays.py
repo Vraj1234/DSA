1b5f8be84dca7a5623870a4708e0b1bee9d83365
Vraj Rajpura <53624234+Vraj1234@users.noreply.github.com>
2026-09-06 16:26:16 -0700

    Time: 7 ms (12.94%) | Memory: 19.5 MB (78.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,28 +1,29 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        res = []
-        l1, l2 = 0,0
-
-        while l1< len(nums1) and l2<len(nums2):
-            if nums1[l1] <= nums2[l2]:
-                res.append(nums1[l1])
-                l1+=1
-            else:
-                res.append(nums2[l2])
-                l2+=1
-            
-        while l1< len(nums1):
-            res.append(nums1[l1])
-            l1+=1
-
-        while l2< len(nums2):
-            res.append(nums2[l2])
-            l2+=1
-        
-        #print(res)
-        if len(res)%2 == 1:
-            return res[len(res)//2]
+        n1 = len(nums1)
+        n2 = len(nums2)
+        if n1<=n2:
+            smaller = nums1
+            larger = nums2
         else:
-            h = res[len(res)//2]
-            l = res[len(res)//2 - 1]
-            return (l+h)/2
+            smaller = nums2
+            larger = nums1
+        
+        l,r = 0, len(smaller)
+        half_size = (n1+n2)//2
+        while l<=r:
+            mid = l + ((r-l)//2)
+            l2 = float('-inf') if mid == 0 else smaller[mid-1]
+            r2 = float('inf') if mid == len(smaller) else smaller[mid]
+            l1 = float('-inf') if half_size-mid-1 < 0 else larger[half_size-mid-1]
+            r1 = float('inf') if half_size-mid == len(larger) else larger[half_size-mid]
+            if l1<=r2 and l2<=r1:
+                if (n1+n2)%2 == 0: #even
+                    return float((max(l1,l2) + min(r1,r2))/2)
+                else:
+                    return float(min(r1,r2))
+                
+            elif l1 > r2:
+                l = mid+1
+            else:
+                r = mid-1
